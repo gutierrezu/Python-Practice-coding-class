bf7c387d03fc56d2565baed9e595b9c1baa5162f
gutierrezu <gutierrezu@wsc.school.nz>
2026-05-07 17:45:19 +1200

    movie question game
    the game's movie is the good, thebad, and the ugly. it uses input and print to function. the diffrence between input and print is that when executed. it shows whatever value that is number or letters on the "view port" while input does the same task but it aditionaly lets the user type whateverl the link in the view port

---
+++ lesson 1 - Output and Input/lesson 1 - Output and input.py
@@ -0,0 +1,12 @@
+input("it is part of a trilogy ")
+print("it is old ")
+input("few people know it ")
+print("fewer people have seen it ")
+input("some say its good ")
+print("other's say its bad ")
+input("but i think...")
+print("its ugly ")
+
+
+
+
