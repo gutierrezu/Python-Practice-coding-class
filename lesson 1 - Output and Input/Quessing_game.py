a3474aeb5176d4554aa43cd4b856b689b7428467
gutierrezu <gutierrezu@wsc.school.nz>
2026-05-10 14:15:40 +1200

    Update Quessing_game.py

--- lesson 1 - Output and Input/Quessing_game.py
+++ lesson 1 - Output and Input/Quessing_game.py
@@ -2,10 +2,12 @@ print("let's see if you can guess THIS movie ")
 hint_1 = input("some say its good. ")
 hint_2 = input("others say its bad. ")
 hint_3 = input("but i think... ")
-input("its ugly. ")
-print("wow! congratulations. you won! and your prize is knowledege...yay")
 
 if hint_1 == "the good the bad and the ugly":
     print("thats correct! how did you get that so soon?")
+
+
+if hint_2 == "the good the bad and the ugly":
+    print("thats correct! how did you get that so soon?")
 else:
-    hint_2
+    print(hint_3)
