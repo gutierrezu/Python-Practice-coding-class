229e9a87d48d59989135040b58d557cced32cb80
gutierrezu <gutierrezu@wsc.school.nz>
2026-05-12 18:16:57 +1200

    working on the questions of the quiz
    the quiz has been updated with if statements. to provide the user with correct or incorrect respoces if he gets a question wrong. i have included an easter egg should the user input "dave" as his name.

--- quiz.py
+++ quiz.py
@@ -4,20 +4,30 @@ if user_name == ("dave"):
     print("I'm sorry, Dave. I'm afraid I can't do that.")
     print("but ill continue.")
     dactyl_question_one = input("dactyl is the moon of what.")
-    if dactyl_question_one == ("asteroid 243 Ida") ("an asteroid"):
-        print("good job.")
+    if dactyl_question_one == ("asteroid 243 Ida") or dactyl_question_one ==("an asteroid"):
+        print("good job. test is now over")
     else:
         print("It can only be attributable to human error.")
 
 else:
-    print("hello " + user_name + ". this is a quiz dedicated to the moons of this solar system.")
-    Moon_question_one = input("have people truly been to the moon?")
-    if Moon_question_one = "yes":
+    print("hello " + user_name + ". this is a quiz dedicated to the moons of this solar system. get all the answers correct and you'l get acaii art as a reward")
+    Moon_question_one = input("have people truly been to the moon? ")
+    if Moon_question_one == "yes":
         print("sure sure buddy")
     else:
         print("sure sure buddy")
-    Moon_question_Two = input("what is the name of the closest moon")
-    if Moon_question_Two = "moon":
-        print("yes!! point for you!! (i won't be c)")
-        
+    Moon_question_Two = input("what is the name of the closest moon ")
+    if Moon_question_Two == "moon":
+        print("yes!! point for you!! (i won't be counting tho)")
+    Moon_question_Three = input("of what planet were the first moons discovered after the moon? ")
+    if Moon_question_Three == ("saturn"):
+        print("that correct!! that's three points? IDK")
+    else:
+        print("nope. minus one point for you!!. ")
+    print("Good job so far " + user_name + " lets see if you get this one.")
+    Moon_question_four = input("how big is charon?")
+    if Moon_question_four == ("half the size of pluto"):
+        print("that is correct!! add one more point.")
+    else:
+        print("should i dum these down for you? you loose one poin.t")
 
