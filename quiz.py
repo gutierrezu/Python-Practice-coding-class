05c70f601d076c0963ef8f21cfb8d73116721d1c
gutierrezu <gutierrezu@wsc.school.nz>
2026-05-12 15:07:20 +1200

    started working on the quiz

---
+++ quiz.py
@@ -0,0 +1,23 @@
+print("what is your name user?")
+user_name = input("")
+if user_name == ("dave"):
+    print("I'm sorry, Dave. I'm afraid I can't do that.")
+    print("but ill continue.")
+    dactyl_question_one = input("dactyl is the moon of what.")
+    if dactyl_question_one == ("asteroid 243 Ida") ("an asteroid"):
+        print("good job.")
+    else:
+        print("It can only be attributable to human error.")
+
+else:
+    print("hello " + user_name + ". this is a quiz dedicated to the moons of this solar system.")
+    Moon_question_one = input("have people truly been to the moon?")
+    if Moon_question_one = "yes":
+        print("sure sure buddy")
+    else:
+        print("sure sure buddy")
+    Moon_question_Two = input("what is the name of the closest moon")
+    if Moon_question_Two = "moon":
+        print("yes!! point for you!! (i won't be c)")
+        
+
