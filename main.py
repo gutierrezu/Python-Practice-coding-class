b84a918bf2fa227e2ecc94a17024434ea0d5664f
gutierrezu <gutierrezu@wsc.school.nz>
2026-05-14 09:19:42 +1200

    try what these code lines one by one. What happen

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 #age = 5
 #print("he is " + str(age))
 
-age = input("age? ")
-print("birth year:", 2026 - int(age))
+#age = input("age? ")
+#print("birth year:", 2026 - int(age))
+
+print(5 + 5 == 10)
+
+print(5+5==8)
+
+print(5==5.0)
+
+print(5+5 >= 10)
