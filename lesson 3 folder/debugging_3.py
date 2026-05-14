9f3c9482d173f3a39ab908f44a27e9db4a621992
gutierrezu <gutierrezu@wsc.school.nz>
2026-05-14 21:28:20 +1200

    Update debugging_3.py

--- lesson 3 folder/debugging_3.py
+++ lesson 3 folder/debugging_3.py
@@ -1,17 +1,19 @@
 # 3 Question Quiz - Debug all the errors, including any semantic errors.
-# write is not a functional python statement. it is meant to be print #
+# write is not a functional python statement. it is meant to be print. this is for line 3#
 print("Hello!")
-# all () statements need to hava a closing( and an ending ) #
+# all () statements need to hava a closing( and an ending ). for line 5 #
 print("This is a quick 3 part quiz. A question will be asked, and then you answer it")
-# the same with "" '' they need a closing and and opening to them.#
-print('First question:")
-input("How many millimetres in a centimetre?)
-print(The answer is 10!)
+# the same with "" '' they need a closing and and opening to them. this is for lines 7,8 and 9 #
+print("First question:")
+input("How many millimetres in a centimetre?")
+print("The answer is 10!")
 
 input("Next Question:")
 print("What is the capital of New Zealand?")
-print('The answer is Wellington. That was easy, wasn't it?')
+# the open and close statement rule aplies here. there are three ' the third is in wasnt  it is better to use "" in this case#
+print("The answer is Wellington. That was easy, wasn't it?")
 
 print("Final Question!")
 input("What is 3 x 6?")
-Print("The answer is 18!")
+# print and other statemetns of the like aperently need to be spelled the same? #
+print("The answer is 18!")
