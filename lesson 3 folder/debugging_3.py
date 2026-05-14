21581c5ff2489548d9e6cf17e80e337c5ef3df49
gutierrezu <gutierrezu@wsc.school.nz>
2026-05-14 21:15:12 +1200

    Create debugging_3.py

---
+++ lesson 3 folder/debugging_3.py
@@ -0,0 +1,17 @@
+# 3 Question Quiz - Debug all the errors, including any semantic errors.
+# write is not a functional python statement. it is meant to be print #
+print("Hello!")
+# all () statements need to hava a closing( and an ending ) #
+print("This is a quick 3 part quiz. A question will be asked, and then you answer it")
+# the same with "" '' they need a closing and and opening to them.#
+print('First question:")
+input("How many millimetres in a centimetre?)
+print(The answer is 10!)
+
+input("Next Question:")
+print("What is the capital of New Zealand?")
+print('The answer is Wellington. That was easy, wasn't it?')
+
+print("Final Question!")
+input("What is 3 x 6?")
+Print("The answer is 18!")
