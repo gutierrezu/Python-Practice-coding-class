6fd5a999a2eef526dbede3249084ca389a2a5e4f
gutierrezu <gutierrezu@wsc.school.nz>
2026-05-23 15:11:40 +1200

    almost finished the agent code.
    i have followed the instructions of the oomments

--- Lesson 4 - If/agent.py
+++ Lesson 4 - If/agent.py
@@ -1,25 +1,36 @@
 ### Secret Agent Login
 # Create a login process for a secret agent
-login = lorem ipsum
+login = "lorem ipsum"
 # Ask for the user's name and save it in a variable
                     #greetings user. insert you code name.#
 users_name = input (".eman edoc uoy tresni .resu sgniteerg ")
 # Ask for the password and save it in a variable
-agents_pasword = input("wellcome agent" + users_name + "good to see you again. you. ")
+agents_pasword = input("wellcome agent" + users_name + "good to see you again. just to comfirm that you are you. "
+"answer this ridle please. it will reveal the pass-code."
+" Africa is where I fly, in open lands and desert sky. Slender build and darkened eye, I’m fierce and swift when prey is nigh. "
+"what am i? ")
 # Check if the password == 'Falcon'
-if agents_pasword == ("falcon")
+if agents_pasword == ("falcon"):
     # Ouput that access has been granted and welcome user using their name
+    print("yes. it is truely you."+ users_name +" welcome to the terinal. a few more segurity to go throught. ")
+else:
+    print("[ERROR. UNAUTHORIZED ACCESSS. POISONOUS GAS RELEASED.] SssSssSssss")
 
     # Ask for the user's age and save it in a variable
-
+users_age = input("what is your age. agent.")
     # Change the age into an integer
-
+users_age = int
     # If the user's age is under 13, tell them they are a spy in training
+if users_age < 13:
+    print("you are no agent. merely in training. sending head quarters your location and notifiing them to update the segurity.  you are unathorized.")
 
     # If their age is under 18, tell them they are a junior spy
+elif users_age < 18:
+    print("you are a junior agent. you are you are unathorized.")
 
     # If their age is 18 or over, tell them they are a Field Agent
-
+else:
+    print("you are a field agent. you are athorized.")
 # Output a goodbye
 
 # ___________________________
